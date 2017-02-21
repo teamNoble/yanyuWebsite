b9a31fe756e25c0a68dc7664319d2a5b7054807e
zhaochenguang <kevin1993@sildev.cn>
2017-02-21 16:40:48 +0800

    update subscribe page and podcast page

--- src/view/Podcast.vue
+++ src/view/Podcast.vue
@@ -1,9 +1,36 @@
 <template>
   <div>
-    这里是主播介绍页面
+    <div class="title row">
+      <div class="intro col-md-4">
+        <img class="head" src="../assets/logo.png">
+        <div class="introText">这里是介绍语句，这里是介绍语句，这里是介绍语句</div>
+      </div>
+      <div class="intro col-md-4">
+        <img class="head" src="../assets/logo.png">
+        <div class="introText">这里是介绍语句，这里是介绍语句，这里是介绍语句</div>
+      </div>
+      <div class="intro col-md-4">
+        <img class="head" src="../assets/logo.png">
+        <div class="introText">这里是介绍语句，这里是介绍语句，这里是介绍语句</div>
+      </div>
+    </div>
   </div>
 </template>
 <style>
+  .title {
+    margin-top: 150px;
+  }
+  .head {
+    border-radius: 180px;
+    height: 300px;
+    border: 1px solid #848484;
+    box-shadow: 6px 6px 3px #888888;
+  }
+  .intro {
+  }
+  .introText {
+    line-height: 50px;
+  }
 </style>
 <script>
   export default{
--- src/view/Subscribe.vue
+++ src/view/Subscribe.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
-    这里是订阅页面
+    <div class="howToSubscribe">
+      <span class="title">1. 如何订阅「言语.萌」播客</span><br>
+      我们推荐您使用通用播客客户端订阅我们的节目<br>
+      在iOS上，我们推荐使用系统<a href="#"> 自带的播客客户端</a>。<br>
+      在windows上，我们推荐<a href="#">groovy podcast</a>客户端<br>
+      在macOS上，我们推荐使用<a href="#"> iTunes</a><br><br>
+      <span class="title">2. 为什么要听播客？</span><br>
+      首先是可以在上下班坐地铁或者公交车、平时走路或者开车的时候聆听来打发时间，<br>
+      其次可以减少面对屏幕和低头的时间。<br>事实上，<br>
+      如果你为了腾出听播客的时间而开始运动，我们会认为自己做了善事。
+    </div>
   </div>
 </template>
 <style>
+  .howToSubscribe {
+    text-align: center;
+    margin: 0 auto;
+    color: inherit;
+    font-size: large;
+    font-weight: bold;
+    line-height: 45px;
+  }
+  .title {
+    font-weight: 400;
+    color: #2c3e50;
+  }
+  a:link {
+    color: #007d84;
+  }
 </style>
 <script>
   export default{
